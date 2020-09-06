0a9920acf85b25b626ff46f8dde6beb7c18ad10b
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-09-06 10:55:47 +0200

    Refactored code

---
+++ src/utils/BookmarkData.js
@@ -0,0 +1,8 @@
+class BookmarkData {
+    constructor(href, date_added, date_modified, title) {
+        this.href = href;
+        this.date_added = date_added;
+        this.date_modified = date_modified;
+        this.title = title;
+    }
+}
