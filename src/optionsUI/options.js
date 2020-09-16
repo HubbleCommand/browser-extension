5e50c8475ae4088c7c65ec2bf701bec3d019b49d
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-09-16 21:53:53 +0200

    Added ability to re-import an exported bookmarks file

--- src/optionsUI/options.js
+++ src/optionsUI/options.js
@@ -28,5 +28,42 @@ function restoreOptions() {
     });
 }
 
+async function handlePicked() {
+    document.getElementById("input").disabled = true;   //First, disable the button to select a file
+    
+    console.log(this.files)
+    var data = await this.files[0].text();
+    data = JSON.parse(data);
+
+    //Create the new folder
+    var createNewFolderRoot = await browser.bookmarks.create({
+        parentId:"toolbar_____",
+        title:"(just imported) " + this.files[0].name,
+        type:"folder"
+    })
+
+    var numberOfItemsToImport = data.length;
+    var counter = 0;
+    console.log("numberOfItemsToImport")
+    console.log(numberOfItemsToImport)
+
+    //Iterate over the bookmarks to import, and create them!
+    for(item of data){
+        await browser.bookmarks.create({
+            parentId:createNewFolderRoot.id,
+            title:item.title,
+            url:item.href
+        })
+        
+        document.getElementById("loadingImportBookmarksProgress").innerHTML = "Import progress : " + counter + " / " + numberOfItemsToImport;
+        counter += 1;
+    }
+    console.log("Done importing!!!");
+    document.getElementById("input").disabled = false;  //re-enable the button
+}
+
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("change", handlePicked, false);
+
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
