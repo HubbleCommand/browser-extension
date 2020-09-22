29c0714ed4c9d7c1c3a2151f8c19a56171e2bc47
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-09-22 12:02:54 +0200

    Exports bookmark with folder path

--- src/background-bookmarks-expimp.js
+++ src/background-bookmarks-expimp.js
@@ -5,21 +5,23 @@ async function downloadBookmarksFolderToFile(start){
     var bookmarks = [];
     var bookmark = await browser.bookmarks.get(start.bookmarkId)
 
-    async function recursivelyGetFolderContents(item){
+    async function recursivelyGetFolderContents(item, path){
+        //console.log(path)
+        //TODO change from type to if has URL, as type is only available in Firefox
         if (item.type == "bookmark"){
             console.log("Got to a bookmark. Adding!")
-            bookmarks.push(new BookmarkData(item.url, item.dateAdded, item.dateGroupModified, item.title))
+            bookmarks.push(new BookmarkData(item.url, item.dateAdded, item.dateGroupModified, item.title, path))
         } else if(item.type == "folder") {
             console.log("Got a folder! getting contents...")
             var folderContents = await browser.bookmarks.getChildren(item.id)
             for (var i = 0; i < folderContents.length; i++){
-                await recursivelyGetFolderContents(folderContents[i])
+                await recursivelyGetFolderContents(folderContents[i], path + "///---rsvp-delim---///" + item.title)
             }
         } else {
             throw "Bookmark item does not have a valid type!"
         }
     }
-    await recursivelyGetFolderContents(bookmark[0])
+    await recursivelyGetFolderContents(bookmark[0], '')
 
     var bookmarksBlob = new Blob([JSON.stringify(bookmarks, null, 2)], {
         type:'application/json'
--- src/utils/BookmarkData.js
+++ src/utils/BookmarkData.js
@@ -1,8 +1,9 @@
 class BookmarkData {
-    constructor(href, date_added, date_modified, title) {
+    constructor(href, date_added, date_modified, title, path) {
         this.href = href;
         this.date_added = date_added;
         this.date_modified = date_modified;
         this.title = title;
+        this.path = path;
     }
 }
